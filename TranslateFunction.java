a4a9cd9b78919aa990c4b73e59fdbea8a30f19a2
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-12-09 14:54:02 -0500

    Update TranslateFunction.java

--- TranslateFunction.java
+++ TranslateFunction.java
@@ -4,6 +4,14 @@ import com.google.cloud.translate.Translation;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Function;
 
+/**
+Sure! Here's an example of a simple Apache Pulsar function written in Java that translates text from Spanish to English using the Google Translate API:
+
+This function takes in a string of text in Spanish as input, translates it using the Google Translate API, and returns the translated text in English.
+
+Note: In order to use the Google Translate API, you will need to set up a Google Cloud Platform account and enable the Translate API. You will also need to set up authentication credentials and add the necessary dependencies to your project. For more information, see the Google Translate API documentation.
+
+*/
 public class TranslateFunction implements Function<String, String> {
  @Override
  public String process(String input, Context context) {
