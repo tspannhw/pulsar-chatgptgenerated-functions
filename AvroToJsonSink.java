e8428d70cce61bee22a7e9ef16023b85f76fae80
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2022-12-10 21:49:11 -0500

    Update AvroToJsonSink.java

--- AvroToJsonSink.java
+++ AvroToJsonSink.java
@@ -77,3 +77,49 @@ export PULSAR_TOKEN=<AUTH_TOKEN>
 export FUNCTION_NAME=<
 
 */
+
+/*
+Here is a sample deploy script for deploying the Pulsar Function that was just created:
+
+*/
+
+#!/bin/bash
+
+# Set the Pulsar broker URL and authentication token
+export PULSAR_BROKER_URL=<BROKER_URL>
+export PULSAR_TOKEN=<AUTH_TOKEN>
+
+# Set the name of the Pulsar Function
+export FUNCTION_NAME=<FUNCTION_NAME>
+
+# Set the JAR file containing the Pulsar Function
+export JAR_FILE=<JAR_FILE_PATH>
+
+# Set the class name of the Pulsar Function
+export CLASS_NAME=<CLASS_NAME>
+
+# Set the HTTP end-point where the Pulsar Function will send JSON messages
+export HTTP_ENDPOINT=<HTTP_ENDPOINT>
+
+# Deploy the Pulsar Function
+pulsar-admin functions create \
+  --tenant public \
+  --namespace default \
+  --name $FUNCTION_NAME \
+  --inputs persistent://public/default/input-topic \
+  --output persistent://public/default/output-topic \
+  --jar $JAR_FILE \
+  --className $CLASS_NAME \
+  --logTopic persistent://public/default/log-topic \
+  --resources memory=1G,cpu=1 \
+  --sinkConfig '{"httpEndpoint": "'$HTTP_ENDPOINT'"}'
+
+This script assumes that you have already set up the necessary Pulsar broker URL and authentication token, 
+as well as the name, JAR file, class name, and HTTP end-point for your Pulsar Function. 
+    You will need to replace the placeholder values in the script with the appropriate values for your specific Pulsar Function.
+
+To use this script, save it to a file with a .sh extension, make the file executable (e.g. with chmod +x <SCRIPT_FILE>), 
+and run it with ./<SCRIPT_FILE>. This will deploy your Pulsar Function to the specified Pulsar cluster.
+
+I hope this helps! Let me know if you have any other questions.      
+      
